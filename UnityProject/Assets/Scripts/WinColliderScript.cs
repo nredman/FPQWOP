e05a18ba2eda5f48e3094fad22bfc1bf5543f7d3
CallumColvine <callumcolvine@gmail.com>
2015-02-08 15:07:52 -0800

    Uhunh, das my shit.. All da girls gon be liek dis.

---
+++ UnityProject/Assets/Scripts/WinColliderScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinColliderScript : MonoBehaviour {
+
+	public string sceneToChangeTo;
+
+	void OnTriggerEnter(Collider hit) {
+
+		//Debug.Log("Inside OnCollisionEnter" + hit);
+
+		if(hit.gameObject.name == "End Game Box"){
+			//Debug.Log("Well you hit the box");
+			Application.LoadLevel(sceneToChangeTo);
+
+		}
+
+	}
+}
