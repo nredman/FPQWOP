e37bce99ac519261acbedd44949b84b4da4db6c8
Neil Redman <nredman@uwaterloo.ca>
2015-02-08 18:06:04 -0500

    Added death detection

---
+++ UnityProject/Assets/Character/DeadDetection.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeadDetection : MonoBehaviour {
+	public string LevelToLoad;
+	// Use this for initialization
+	public void goToNewLevel () {
+		Application.LoadLevel(LevelToLoad);
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		Debug.Log (collision.gameObject.tag);
+		if (collision.gameObject.tag == "floor"){
+			goToNewLevel();
+			Debug.Log ("head hit ground");
+		}
+	}
+}
