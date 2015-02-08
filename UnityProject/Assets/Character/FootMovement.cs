8db0e3985da425b267029df4ae87680de7652416
Neil Redman <nredman@uwaterloo.ca>
2015-02-07 20:18:20 -0500

    Another pass on leg scripts

---
+++ UnityProject/Assets/Character/FootMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Move(Vector2 mov){
+		Vector3 targetVector = new Vector3 ();
+		targetVector.x = mov.x;
+		targetVector.y = 0;
+		targetVector.z = mov.y;
+		rigidbody.AddForce (targetVector);
+	}
+}
+
+
+
+
---
+++ UnityProject/Assets/Character/UpperLegScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpperLegScript : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		Lift (Time.deltaTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Lift (Time.deltaTime);
+	}
+
+	void Lift(float amount){
+		Debug.Log (amount);
+		Vector3 eulerRot = this.GetComponent<ConfigurableJoint> ().targetRotation.eulerAngles;
+		eulerRot.x += amount;
+		Debug.Log (eulerRot.ToString());
+		Quaternion rot = Quaternion.Euler (eulerRot);
+		Debug.Log (rot.ToString ());
+		GetComponent<ConfigurableJoint> ().targetRotation = rot;
+	}
+}
